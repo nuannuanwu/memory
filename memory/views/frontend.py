30e3b385d7a09b2d162c6ffbc7cba4766e3498a2
panrj <panrj@e-co.com.cn>
2013-03-14 11:19:27 +0800

    pic

--- memory/views/frontend.py
+++ memory/views/frontend.py
@@ -4,10 +4,10 @@ from easy_thumbnails.fields import ThumbnailerImageField
 from memory.models import Profile
 
 def index(request, template_name="memory/memory_index.html"):
-    profile = get_object_or_404(Profile, pk=1)
-    thumb_url = profile.photo['avatar'].url
-    print thumb_url,'uuuuuuuuuuuuuu'
-    return render(request, template_name,{"thumb_url":thumb_url})
+    #profile = get_object_or_404(Profile, pk=1)
+    #thumb_url = profile.photo['avatar'].url
+    #print thumb_url,'uuuuuuuuuuuuuu'
+    return render(request, template_name)
 
 def test_index(request, template_name="memory/test_index.html"):
     space = range(30)
